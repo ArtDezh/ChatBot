e984ee87b52f3853480bcf6abb76ff9b0daeb305
Arthur <artur2134@mail.ru>
2024-07-07 13:59:42 +0300

    added bot's logical

--- src/SimpleBot.java
+++ src/SimpleBot.java
@@ -1,4 +1,6 @@
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.regex.Pattern;
 
@@ -27,6 +29,68 @@ public class SimpleBot {
             "Уверен, Вы уже догадались сами.",
             "Зачем Вам такая информация?",
             "Давайте сохраним интригу?"};
+
+    final Map<String, String> PATTERNS_FOR_BOT = new HashMap<String, String>() {{
+        //прветствие
+        put("хай", "hello");
+        put("привет", "hello");
+        put("здорово", "hello");
+        put("здравствуй", "hello");
+
+        //кто ты?
+        put("кто\\s.*ты", "who");
+        put("ты\\s.*кто", "who");
+
+        //имя?
+        put("как\\s.*зовут", "name");
+        put("как\\s.*имя", "name");
+        put("есть\\s.*имя", "name");
+        put("какое\\s.*имя", "name");
+
+        // как дела?
+        put("как\\s.*дела", "howareyou");
+        put("как\\s.*жизнь", "howareyou");
+
+        // что делаешь?
+        put("зачем\\s.*тут", "whatareyoudoing");
+        put("зачем\\s.*здесь", "whatareyoudoing");
+        put("что\\s.*делаешь", "whatareyoudoing");
+        put("чем\\s.*занимаешься", "whatareyoudoing");
+
+        // что нравится?
+        put("что\\s.*нравится", "whatdoyoulike");
+        put("что\\s.*любишь", "whatdoyoulike");
+
+        //чувства
+        put("кажется", "iamfeelling");
+        put("чувствую", "iamfeelling");
+        put("испытываю", "iamfeelling");
+
+        // да(согласие)
+        put("^да", "yes");
+        put("согласен", "yes");
+
+        //сколько время?
+        put("который\\s.*час", "whattime");
+        put("сколько\\s.*время", "whattime");
+
+        //пока
+        put("прощай", "bye");
+        put("увидимся", "bye");
+        put("до\\s.*свидания", "bye");
+    }};
+
+    final Map<String, String> ANSWERS_BY_PATTERNS = new HashMap<String, String>(){{
+        put("hello", "Здравствуйте, рад Вас видеть.");
+        put("who", "Я обычный чат.");
+        put("name", "Зовите меня Ботик.");
+        put("howareyou", "Спасибо, что интересуетесь. Уменя все хорошо.");
+        put("whatareyoudoing", "Я пробую общаться с людьми.");
+        put("whatdoyoulike", "Мне нравится думать, что я не просто программа.");
+        put("iamfeelling", "Как давно это началось? Расскажите об этом подробнее.");
+        put("yes", "Согласие есть продукт при полном непротивлении сторон.");
+        put("bye", "До свидания! Надеюсь, еще увидимся.");
+    }};
     Pattern pattern;
     Random random;
     Date date;
@@ -37,7 +101,17 @@ public class SimpleBot {
     }
 
     String sayInReturn(String msg, boolean ai) {
-        return (msg.trim().endsWith("?")) ? ELUSIVE_ANSWERS[random.nextInt(ELUSIVE_ANSWERS.length)] :
+        String say = (msg.trim().endsWith("?")) ? ELUSIVE_ANSWERS[random.nextInt(ELUSIVE_ANSWERS.length)] :
                 COMMON_PHRASES[random.nextInt(COMMON_PHRASES.length)];
+        if (ai) {
+            String message = String.join(" ", msg.toLowerCase().split("[ (,!.)?]+"));
+            for (Map.Entry<String,String> o: PATTERNS_FOR_BOT.entrySet()) {
+                pattern = Pattern.compile(o.getKey());
+                if (pattern.matcher(message).find())
+                    if (o.getValue().equals("whattime")) return date.toString();
+                    else return ANSWERS_BY_PATTERNS.get(o.getValue());
+            }
+        }
+        return say;
     }
 }
