ddfb576c987c16d708af47e058900cd795e2942a
Arthur <artur2134@mail.ru>
2024-07-07 11:15:13 +0300

    creat window of program

--- src/ChatBoot.java
+++ src/ChatBoot.java
@@ -1,5 +1,27 @@
-public class ChatBoot {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ChatBoot extends JFrame {
+    final String TITLE_OF_PROGRAM = "Simple ChatBot";
+    final int START_LOATION = 200;
+    final int WINDOW_WIDTH = 350;
+    final int WINDOW_HEIGHT = 450;
+
+    JTextArea dialogue; //место для диалога
+    JCheckBox ai; // пдключить ai
+    JTextField message; // поле для ввода сообщения
+    //SimpleBot sbot;
+
     public static void main(String[] args) {
-       
+        new ChatBoot();
+    }
+
+    public ChatBoot() {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(START_LOATION, START_LOATION, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        setVisible(true);
     }
 }
